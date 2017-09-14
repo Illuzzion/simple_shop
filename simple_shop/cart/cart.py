7b6e4f99ce492ed8e37b3f350e7fd9fa7728ff1f
Illuzzion <79fz270704@gmail.com>
2017-09-14 10:49:25 +0300

    add simple cart

--- simple_shop/cart/cart.py
+++ simple_shop/cart/cart.py
@@ -1,49 +1,58 @@
 from decimal import Decimal
 
 from django.conf import settings
-
 from shop.models import Product
 
 
 class Cart(object):
+    """
+    Корзина
+    """
+
     def __init__(self, request):
-        # Инициализация корзины пользователя
         self.session = request.session
-        self.cart = self.session.get(settings.CART_SESSION_ID, {})
+        self.cart = self.session.get(settings.CART_SESSION_ID, dict())
 
-    # Добавление товара в корзину пользователя или обновление количества товара
     def add(self, product, quantity=1, update_quantity=False):
-        product_id = str(product.id)
+        """
+        Добавление товара в корзину
 
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        # default_value = {'quantity': 0, 'price': str(product.price)}
-        # product = self.cart.get(str(product.id), default_value)
-        # product['quantity'] = quantity if update_quantity else product['quantity'] + quantity
-
+        :param product: Товар
+        :param quantity: Количество
+        :param update_quantity: Флаг обновления количества
+        :return: None
+        """
+        product_dict = self.cart.get(product.id, {'price': str(product.price), 'quantity': 0})
+        product_dict['quantity'] = quantity if update_quantity else product_dict['quantity'] + quantity
+        # когда сохраняется сессия, все _ключи_ типа int преобразуются в строки
+        # https://djbook.ru/rel1.9/topics/http/sessions.html#django.contrib.sessions.serializers.JSONSerializer
+        self.cart.update({product.id: product_dict})
         self.save()
 
-    # Сохранение данных в сессию
     def save(self):
+        """
+        Сохранение данных корзины в сессию
+
+        :return:
+        """
         self.session[settings.CART_SESSION_ID] = self.cart
-        # Указываем, что сессия изменена
         self.session.modified = True
 
     def remove(self, product):
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
+        """
+        Удаление товара из корзины
+
+        :param product: Товар который нужно удалить
+        :return:
+        """
+        if str(product.id) in self.cart:
+            del self.cart[str(product.id)]
             self.save()
 
     # Итерация по товарам
     def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
+        # product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=self.cart.keys())
 
         for product in products:
             self.cart[str(product.id)]['product'] = product
@@ -61,5 +70,9 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
+        """
+        Очистка корзины
+        :return:
+        """
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
